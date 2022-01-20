47a7e03f688d2c4b6d3203b10e61bdd1a56dfc3b
Emily Tamayo <emilytamayo@R2Hs-MBP.lan>
2022-01-19 23:15:41 -0500

    Solved it

--- hard/script.js
+++ hard/script.js
@@ -1,30 +1,36 @@
 let tom = {
   height: 9,
   mass: 8,
-  bmi: function () {
-    console.log(this.mass / (this.height * this.height));
-  },
+  //bmi: function () {
+  // console.log(this.mass / (this.height * this.height));
+  //},
 };
-tom.bmi();
+//tom.bmi();
 
 let jerry = {
   height: 10,
   mass: 45,
-  bmi: function () {
-    console.log(this.mass / (this.height * this.height));
-  },
+  //bmi: function () {
+  //console.log(this.mass / (this.height * this.height));
+  //},
 };
 
-jerry.bmi();
+//jerry.bmi();
 
-let verdict = false;
-
-if (tomBmi < jerryBmi) {
-  console.log(`Is Tom's BMI higher than Jwrry's? ${verdict}`);
+function bmiCheck(mass, height) {
+  let check = mass / (height * height);
+  return check;
 }
 
-//function bmiCheck(height, mass) {
-//return mass / (height * height);
-//}
+console.log(bmiCheck(jerry.mass, jerry.height));
+console.log(bmiCheck(tom.mass, tom.height));
+
+let tomBMI = bmiCheck(tom.mass, tom.height);
+let jerryBMI = bmiCheck(jerry.mass, jerry.height);
+let verdict = false;
+
+if (tomBMI < jerryBMI) {
+  console.log(`Is Tom's BMI higher than Jwrry's? ${verdict}`);
+}
 //console.log(`Is Tom's BMI higher than Jerry's? ${num}`);
 //console.log(bmiCheck());
