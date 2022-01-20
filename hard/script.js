912c0ab417901e9558bcb4197776fee8efe03d41
Emily Tamayo <emilytamayo@R2Hs-MBP.lan>
2022-01-19 22:46:42 -0500

    trying to solve hard and figure out how to get my boolean to work, dont mind me.

--- hard/script.js
+++ hard/script.js
@@ -1,18 +1,30 @@
 let tom = {
   height: 9,
   mass: 8,
+  bmi: function () {
+    console.log(this.mass / (this.height * this.height));
+  },
 };
+tom.bmi();
+
 let jerry = {
   height: 10,
   mass: 45,
+  bmi: function () {
+    console.log(this.mass / (this.height * this.height));
+  },
 };
 
-function bmiCheck(height, mass) {
-  let bmi = (mass / height) * height;
+jerry.bmi();
 
-  return bmi;
+let verdict = false;
+
+if (tomBmi < jerryBmi) {
+  console.log(`Is Tom's BMI higher than Jwrry's? ${verdict}`);
 }
-console.log(bmiCheck());
-let num = false;
 
+//function bmiCheck(height, mass) {
+//return mass / (height * height);
+//}
 //console.log(`Is Tom's BMI higher than Jerry's? ${num}`);
+//console.log(bmiCheck());
